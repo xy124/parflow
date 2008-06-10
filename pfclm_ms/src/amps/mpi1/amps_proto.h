8b322bc7712f5325dbb30d8708004cf3415720db
Steven Smith <smith84@llnl.gov>
2008-06-10 23:49:33 +0000

    Added initial start of WRF wrapper:
    git-svn-id: https://parflow.svn.cloudforge.com/parflow/pfclm_ms/branches/smith84@38 39af3fb0-ca4e-0410-adf9-b4ae76d04516

--- pfclm_ms/src/amps/mpi1/amps_proto.h
+++ pfclm_ms/src/amps/mpi1/amps_proto.h
@@ -43,6 +43,7 @@ amps_File amps_Fopen P((char *filename , char *type ));
 /* amps_init.c */
 int MAIN__ P((void ));
 int amps_Init P((int *argc , char **argv []));
+int amps_EmbeddedInit P((void));
 
 /* amps_invoice.c */
 void amps_AppendInvoice P((amps_Invoice *invoice , amps_Invoice append_invoice ));
