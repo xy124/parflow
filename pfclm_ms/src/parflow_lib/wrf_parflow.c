d9575405a74882fd00cb96cca2c8f4df1f72a2af
Steven Smith <smith84@llnl.gov>
2008-06-14 00:30:39 +0000

    Updated tux config to use OpenMPI
    git-svn-id: https://parflow.svn.cloudforge.com/parflow/pfclm_ms/branches/smith84@55 39af3fb0-ca4e-0410-adf9-b4ae76d04516

--- pfclm_ms/src/parflow_lib/wrf_parflow.c
+++ pfclm_ms/src/parflow_lib/wrf_parflow.c
@@ -18,107 +18,99 @@
 amps_ThreadLocalDcl(PFModule *, Solver_module);
 amps_ThreadLocalDcl(PFModule *, solver);
 
-amps_ThreadLocalDcl(int, parflow_first_time = 1);
-
 void wrfparflowinit_() {
+   // SGS this needs to come from somewhere
+   char *input_file = "sgs_richards_test";
+
    printf("Hello from WRFParflowInit\n");
+
+   /* Begin of main includes */
+
+   /*-----------------------------------------------------------------------
+    * Initialize AMPS 
+    *-----------------------------------------------------------------------*/
+   
+   // SGS this is wrong; need initialize from existing MPI state.
+   if (amps_EmbeddedInit())
+   {
+      amps_Printf("Error: initalization failed\n");
+      exit(1);
+   }
+   
+   /*-----------------------------------------------------------------------
+    * Set up globals structure
+    *-----------------------------------------------------------------------*/
+   NewGlobals(input_file);
+   
+   /*-----------------------------------------------------------------------
+    * Read the Users Input Deck
+    *-----------------------------------------------------------------------*/
+   amps_ThreadLocal(input_database) = IDB_NewDB(GlobalsInFileName);
+   
+   /*-----------------------------------------------------------------------
+    * Setup log printing
+    *-----------------------------------------------------------------------*/
+   NewLogging();
+   
+   /*-----------------------------------------------------------------------
+    * Setup timing table
+    *-----------------------------------------------------------------------*/
+   NewTiming();
+   
+   /* End of main includes */
+   
+   /* Begin of Solver includes */
+   
+   GlobalsNumProcsX = GetIntDefault("Process.Topology.P", 1);
+   GlobalsNumProcsY = GetIntDefault("Process.Topology.Q", 1);
+   GlobalsNumProcsZ = GetIntDefault("Process.Topology.R", 1);
+   
+   GlobalsNumProcs = amps_Size(amps_CommWorld);
+   
+   GlobalsBackground = ReadBackground();
+   
+   GlobalsUserGrid = ReadUserGrid();
+   
+   SetBackgroundBounds(GlobalsBackground, GlobalsUserGrid);
+   
+   GlobalsMaxRefLevel = 0;
+   
+   amps_ThreadLocal(Solver_module) = PFModuleNewModule(SolverRichards, ("Solver"));
+   
+   amps_ThreadLocal(solver) = PFModuleNewInstance(amps_ThreadLocal(Solver_module), ());
+   
+   /* End of solver includes */
+   
+   SetupRichards(amps_ThreadLocal(solver));
 }
 
 void wrfparflowadvance_() 
 {
-   // SGS this needs to come from somewhere
-   char *input_file = "sgs_richards_test";
-
-   if(parflow_first_time) {
-
-      /* Begin of main includes */
-
-      /*-----------------------------------------------------------------------
-       * Initialize AMPS 
-       *-----------------------------------------------------------------------*/
-
-      // SGS this is wrong; need initialize from existing MPI state.
-      if (amps_EmbeddedInit())
-      {
-	 amps_Printf("Error: initalization failed\n");
-	 exit(1);
-      }
-
-      /*-----------------------------------------------------------------------
-       * Set up globals structure
-       *-----------------------------------------------------------------------*/
-      NewGlobals(input_file);
-
-      /*-----------------------------------------------------------------------
-       * Read the Users Input Deck
-       *-----------------------------------------------------------------------*/
-      amps_ThreadLocal(input_database) = IDB_NewDB(GlobalsInFileName);
-
-      /*-----------------------------------------------------------------------
-       * Setup log printing
-       *-----------------------------------------------------------------------*/
-      NewLogging();
-
-      /*-----------------------------------------------------------------------
-       * Setup timing table
-       *-----------------------------------------------------------------------*/
-      NewTiming();
-
-      /* End of main includes */
-
-      /* Begin of Solver includes */
-
-      GlobalsNumProcsX = GetIntDefault("Process.Topology.P", 1);
-      GlobalsNumProcsY = GetIntDefault("Process.Topology.Q", 1);
-      GlobalsNumProcsZ = GetIntDefault("Process.Topology.R", 1);
-
-      GlobalsNumProcs = amps_Size(amps_CommWorld);
-
-      GlobalsBackground = ReadBackground();
-
-      GlobalsUserGrid = ReadUserGrid();
-
-      SetBackgroundBounds(GlobalsBackground, GlobalsUserGrid);
-
-      GlobalsMaxRefLevel = 0;
-
-      amps_ThreadLocal(Solver_module) = PFModuleNewModule(SolverRichards, ("Solver"));
-
-      amps_ThreadLocal(solver) = PFModuleNewInstance(amps_ThreadLocal(Solver_module), ());
-
-      /* End of solver includes */
-
-      SetupRichards(amps_ThreadLocal(solver));
-
-      parflow_first_time = 0;
-
-   } else {
-      double        start_time          = 0.0;
-      double        stop_time           = 0.001;
-      double        dt                  = 0.0;
-
-      Grid         *grid;
-
-      Vector       *pressure_out;
-      Vector       *porosity_out;
-      Vector       *saturation_out;
-
-      Vector       *evap_trans;
-
-      /* Create the flow grid */
-      grid = CreateGrid(GlobalsUserGrid);
-
-      evap_trans = NewVector( grid, 1, 1 );
-      InitVectorAll(evap_trans, 0.0);
-
-      AdvanceRichards(amps_ThreadLocal(solver),
-		      start_time, 
-		      stop_time, 
-		      dt, 
-		      evap_trans,
-		      &pressure_out, 
-		      &porosity_out,
-		      &saturation_out);
-
-   }
+   // SGS These need to come from arguments; passed by WRF
+   double        start_time          = 0.0;
+   double        stop_time           = 0.001;
+   double        dt                  = 0.0;
+   
+   Grid         *grid;
+   
+   Vector       *pressure_out;
+   Vector       *porosity_out;
+   Vector       *saturation_out;
+   
+   Vector       *evap_trans;
+   
+   /* Create the flow grid */
+   grid = CreateGrid(GlobalsUserGrid);
+   
+   evap_trans = NewVector( grid, 1, 1 );
+   InitVectorAll(evap_trans, 0.0);
+   
+   AdvanceRichards(amps_ThreadLocal(solver),
+		   start_time, 
+		   stop_time, 
+		   dt, 
+		   evap_trans,
+		   &pressure_out, 
+		   &porosity_out,
+		   &saturation_out);
 }
