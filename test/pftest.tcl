1f25568bcc0357edd54c8e4d8c6227c3c98fd0cf
Steven Smith <smith84@llnl.gov>
2008-07-25 01:08:03 +0000

    Fixed test script when file not present
    git-svn-id: https://parflow.svn.cloudforge.com/parflow/parflow/branches/smith84@187 39af3fb0-ca4e-0410-adf9-b4ae76d04516

--- test/pftest.tcl
+++ test/pftest.tcl
@@ -1,10 +1,9 @@
 set sig_digits 6
 
 proc pftestFile {file message sig_digits} {
-    set correct [pfload correct_output/$file]
-    set new     [pfload                $file]
-
     if [file exists $file] {
+	set correct [pfload correct_output/$file]
+	set new     [pfload                $file]
 	set diff [pfmdiff $new $correct $sig_digits]
 	if {[string length $diff] != 0 } {
 	    puts "FAILED : $message $diff"
@@ -13,7 +12,7 @@ proc pftestFile {file message sig_digits} {
 	    return 1
 	}
     } {
-	puts "FAILED : output file <$new> not created"
+	puts "FAILED : output file <$file> not created"
 	return 1
     }
 }
