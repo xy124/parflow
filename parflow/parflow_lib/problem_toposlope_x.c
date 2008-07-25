24a760d6ed54394517d1a9c3d88a281bb04df365
Steven Smith <smith84@llnl.gov>
2008-07-25 19:51:03 +0000

    Fixed some warnings reported by insure++
    git-svn-id: https://parflow.svn.cloudforge.com/parflow/parflow/branches/smith84@195 39af3fb0-ca4e-0410-adf9-b4ae76d04516

--- parflow/parflow_lib/problem_toposlope_x.c
+++ parflow/parflow_lib/problem_toposlope_x.c
@@ -435,7 +435,7 @@ PFModule  *XSlopeNewPublicXtra()
 	    
 	    num_regions = (dummy0 -> num_regions) = NA_Sizeof(dummy0 -> regions);
 	    
-	    (dummy0 -> region_indices) = (int *)calloc((unsigned int)(num_regions), (unsigned int)sizeof(int)); 
+	    (dummy0 -> region_indices) = ctalloc(unsigned int, num_regions);
 	    (dummy0 -> values)         = ctalloc(double, num_regions);
 	    
 	    for (ir = 0; ir < num_regions; ir++)
