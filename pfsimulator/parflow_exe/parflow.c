26900703b846f976676287af18874f084dd95b54
Steven Smith <smith84@llnl.gov>
2010-01-05 16:59:36 +0000

    Additional SAMRAI initialization
    git-svn-id: https://parflow.svn.cloudforge.com/parflow/parflow/branches/samrai@501 39af3fb0-ca4e-0410-adf9-b4ae76d04516

--- pfsimulator/parflow_exe/parflow.c
+++ pfsimulator/parflow_exe/parflow.c
@@ -33,8 +33,17 @@
 
 #include "SAMRAI/SAMRAI_config.h"
 
+// Headers for basic SAMRAI classes
+#include "SAMRAI/appu/VisItDataWriter.h"
+#include "SAMRAI/geom/CartesianGridGeometry.h"
+#include "SAMRAI/hier/PatchHierarchy.h"
 #include "SAMRAI/tbox/SAMRAIManager.h"
+#include "SAMRAI/tbox/Database.h" 
+#include "SAMRAI/tbox/InputDatabase.h" 
+#include "SAMRAI/tbox/InputManager.h" 
+#include "SAMRAI/tbox/Utilities.h"
 #include "SAMRAI/tbox/SAMRAI_MPI.h"
+#include "SAMRAI/tbox/RestartManager.h"
 
 #include "parflow.h"
 
@@ -79,11 +88,109 @@ int main (int argc , char *argv [])
    
    wall_clock_time = amps_Clock();
 
-   if(argc != 2) {
-      amps_Printf("Error: invalid number of arguments.\n");
-      amps_Printf("       Invoke as parflow <pfidb file>.\n");
-      exit(1);
+   /*-----------------------------------------------------------------------
+    * Command line arguments
+    *-----------------------------------------------------------------------*/
+   std::string input_filename = "samrai.input";
+   std::string restart_read_dirname;
+   bool is_from_restart = false;
+   int restore_num = 0;
+
+   if ( (argc != 2) && (argc != 4) ) {
+      tbox::pout << "USAGE:  " << argv[0] << " <input pfidb filename> "
+		 << "<restart dir> <restore number>\n"
+		 << std::endl;
+      tbox::SAMRAI_MPI::abort();
+      return (-1);
+   } else {
+      if (argc == 4) {
+	 restart_read_dirname = argv[2];
+	 restore_num = atoi(argv[3]);
+	 
+	 is_from_restart = true;
+      }
    }
+
+   /*-----------------------------------------------------------------------
+    * SAMRAI initialization.
+    *-----------------------------------------------------------------------*/
+
+   /*-----------------------------------------------------------------------
+    * Create input database and parse all data in input file.  
+    *-----------------------------------------------------------------------*/
+   tbox::Pointer<tbox::Database> input_db(new tbox::InputDatabase("input_db"));
+   tbox::InputManager::getManager()->parseInputFile(input_filename, input_db);
+
+   /*
+    * Parse input for options to control logging, visualization and restart.
+    */
+   tbox::Pointer<tbox::Database> main_db = input_db->getDatabase("Main");
+
+   std::string log_file_name = "life.log";
+   if (main_db->keyExists("log_file_name")) {
+      log_file_name = main_db->getString("log_file_name");
+   }
+   bool log_all_nodes = false;
+   if (main_db->keyExists("log_all_nodes")) {
+      log_all_nodes = main_db->getBool("log_all_nodes");
+   }
+   if (log_all_nodes) {
+      tbox::PIO::logAllNodes(log_file_name);
+   } else {
+      tbox::PIO::logOnlyNodeZero(log_file_name);
+   }
+
+   int viz_dump_interval = 0;
+   if (main_db->keyExists("viz_dump_interval")) {
+      viz_dump_interval = main_db->getInteger("viz_dump_interval");
+   }
+   
+   tbox::Array<std::string> viz_writer(1);
+   std::string viz_dump_dirname;
+   int visit_number_procs_per_file = 1;
+   if ( viz_dump_interval > 0 ) {
+      if (main_db->keyExists("viz_dump_dirname")) {
+	 viz_dump_dirname = main_db->getStringWithDefault(
+	    "viz_dump_dirname", "./visit");
+      }
+   }
+   
+   const bool viz_dump_data = (viz_dump_interval > 0);
+   
+   int restart_interval = 0;
+   if (main_db->keyExists("restart_interval")) {
+      restart_interval = main_db->getInteger("restart_interval");
+   } 
+   
+   std::string restart_write_dirname;
+   if ( restart_interval > 0 ) {
+      if (main_db->keyExists("restart_write_dirname")) {
+	 restart_write_dirname = main_db->getString("restart_write_dirname");
+      } else {
+	 TBOX_ERROR("restart_interval > 0, but key `restart_write_dirname'"
+		    << " not specifed in input file");
+      }
+   }
+
+   /*-----------------------------------------------------------------------
+    * Initial logging info on what was run.
+    *-----------------------------------------------------------------------*/
+   tbox::plog << "input_filename = " << input_filename << std::endl;
+   tbox::plog << "restart_read_dirname = " << restart_read_dirname << std::endl;
+   tbox::plog << "restore_num = " << restore_num << std::endl;
+   
+   /*-----------------------------------------------------------------------
+    * If run is from restart then open the restart file.
+    *-----------------------------------------------------------------------*/
+   tbox::RestartManager* restart_manager = tbox::RestartManager::getManager();
+
+   if (is_from_restart) {
+      restart_manager->
+	 openRestartFile(restart_read_dirname, restore_num, 
+			 tbox::SAMRAI_MPI::getNodes() );
+   }
+
+
 
    /*-----------------------------------------------------------------------
     * Set up globals structure
